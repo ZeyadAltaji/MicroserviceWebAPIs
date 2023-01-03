c0e99b0f03994a4813e200c505fc7369e96c2bb4
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-01-03 16:58:46 +0300

    buliding to Layers to Services Producte

--- Gateway.WebAPIs/Program.cs
+++ Gateway.WebAPIs/Program.cs
@@ -4,3 +4,12 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+
+// Download From NuGet Packages 
+// MediatR
+// MediatR.Extensions.Microsoft.Dependencylnjection
+//Microsoft.EntityFrameworkCore
+//Microsoft.EntityFrameworkCore.Relational + sqlServer + Postman\ swagger 
+
